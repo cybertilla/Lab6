0c63e5397615fd127924a1d2f3b78b65974ba10f
cybertilla <bertilla.fabris@gmail.com>
2022-05-12 15:57:33 +0200

    Title and List add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieForm from "./MovieForm";
+import ListOfMovies from "./ListOfMovies";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <h1>Movie Finder</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A collection of movies
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <MovieForm />
+      <ListOfMovies />
     </div>
   );
 }
---
+++ my-app/src/MovieForm.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef } from 'react';
+
+export default function MovieForm() {
+    const [movies, setMovie] = useState([{
+        id: 1,
+        title: "wat",
+        grade: 0
+    }]);
+
+    return (
+        <div>
+            <form>
+            <div class="mb-3">
+                <label class="form-label">Title</label>
+                <input type="text" class="form-control" id="movie-title"></input>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Select rating</label>
+                <select class="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+                <option value="4">Four</option>
+                <option value="5">Five</option>
+            </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            
+            </form>
+        </div>
+    )
+};
