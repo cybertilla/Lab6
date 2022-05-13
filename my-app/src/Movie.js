d5a8a8c42d465aa39c3773037ea98e672747fbd5
cybertilla <bertilla.fabris@gmail.com>
2022-05-13 11:48:52 +0200

    repeat break

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -1,6 +1,12 @@
 import React from 'react'
+//import star from './/images/star.png';
+
 
 export default function Movie(props) {
+    const n = props.item.grade;
+
+    [...Array(n)].map((e, i) => <span className="busterCards" key={i}>♦</span>)
+
     return (
         <li className="list-group-item">
             {  props.item.title }
